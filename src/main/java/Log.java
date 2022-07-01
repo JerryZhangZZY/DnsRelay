eb272feb40548cadc21c09700a8ee2cb06709336
ZaitianWang <wangzaitian@outlook.com>
2022-07-01 18:06:39 +0800

    add log, #5

---
+++ src/main/java/Log.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Log {
+    private final String path = "log.txt";
+    private File log;
+    private String buf = "";
+    public Log() {
+        log = new File(path);
+        if (!log.exists()) {
+            try {
+                log.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void addLog(String logPiece) {
+        System.out.println(logPiece);
+        buf = buf.concat("\t").concat(logPiece).concat("\n");
+    }
+    synchronized public void writeLog() {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+        String logItem = "[" + formatter.format(date) + "]" + buf + "\n";
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(log, true));
+            bw.write(logItem);
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Log log1 = new Log();
+        Log log2 = new Log();
+        log1.addLog("test1a");
+        log1.addLog("test1b");
+        log2.addLog("test2a");
+        log1.addLog("test1c");
+        log2.writeLog();
+        log1.writeLog();
+    }
+}
